e1d8779aa0c7e67f315736bd83433ae945132e54
Steve Yen <steve.yen@gmail.com>
2012-01-11 22:33:50 -0800

    Filter some keys to ignore.

--- cook.js
+++ cook.js
@@ -2,6 +2,16 @@
 
 var fs = require("fs");
 
+var ignore = { 'buildinfo.clusterCompatibility': 1,
+               'buildinfo.mcdMemoryReserved': 1,
+               'buildinfo.mcdMemoryAllocated': 1,
+               'buildinfo.memoryFree': 1,
+               'buildinfo.memoryTotal': 1,
+               'buildinfo.memoryQuota': 1,
+               'info.test_time': 1,
+               'row': 1,
+               'size': 1 }
+
 var o = console.log
 
 o("<html>");
@@ -23,13 +33,15 @@ function process_dir(dir) {
       for (var i in rows) {
         var row = rows[i];
         for (var key in row) {
-          var val = row[key];
-          var key_stat = keys[key] = keys[key] || {};
-          key_stat.count = (key_stat.count || 0) + 1;
-          key_stat.type  = (key_stat.type || typeof(val));
-          if (key_stat.type == "number") {
-            key_stat.min = Math.min(val, key_stat.min || Math.pow(2, 52));
-            key_stat.max = Math.max(val, key_stat.max || 0);
+          if (!ignore[key]) {
+            var val = row[key];
+            var key_stat = keys[key] = keys[key] || {};
+            key_stat.count = (key_stat.count || 0) + 1;
+            key_stat.type  = (key_stat.type || typeof(val));
+            if (key_stat.type == "number") {
+              key_stat.min = Math.min(val, key_stat.min || Math.pow(2, 52));
+              key_stat.max = Math.max(val, key_stat.max || 0);
+            }
           }
         }
       }
