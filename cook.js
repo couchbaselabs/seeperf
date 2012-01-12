d8e0269f4338ca05f5bd2a5fd8b5fabc5cc97e36
Steve Yen <steve.yen@gmail.com>
2012-01-11 18:51:43 -0800

    Simpler data_file looping.

--- cook.js
+++ cook.js
@@ -7,35 +7,33 @@ process.argv.slice(2).forEach(function(dir) {
   });
 
 function process_dir(data_path) {
-  var data_files = fs.readdirSync(data_path);
+  fs.readdirSync(data_path).forEach(function(data_file) {
+      console.log(data_file);
 
-  for (var i in data_files) {
-    console.log(data_files[i]);
+      var data = fs.readFileSync(data_path + "/" + data_file);
+      var json = JSON.parse(data);
 
-    var data = fs.readFileSync(data_path + "/" + data_files[i]);
-    var json = JSON.parse(data);
-
-    var n = 0;
-    var keys = {};
-    for (var j in json) {
+      var n = 0;
+      var keys = {};
+      for (var j in json) {
         n = n + 1;
         var entry = json[j];
         for (var key in entry) {
-            var val = entry[key];
-            var info = keys[key] = keys[key] || {};
-            info.count = (info.count || 0) + 1;
-            info.type  = (info.type || typeof(val));
-            if (info.type == "number") {
-              info.min = Math.min(val, info.min || Math.pow(2, 52));
-              info.max = Math.max(val, info.max || 0);
-            }
+          var val = entry[key];
+          var info = keys[key] = keys[key] || {};
+          info.count = (info.count || 0) + 1;
+          info.type  = (info.type || typeof(val));
+          if (info.type == "number") {
+            info.min = Math.min(val, info.min || Math.pow(2, 52));
+            info.max = Math.max(val, info.max || 0);
+          }
         }
-    }
+      }
 
-    console.log("  bytes   : " + data.length);
-    console.log("  entries : " + n);
-    console.log(keys);
-  }
+      console.log("  bytes   : " + data.length);
+      console.log("  entries : " + n);
+      console.log(keys);
+    });
 }
 
 console.log("DONE")
