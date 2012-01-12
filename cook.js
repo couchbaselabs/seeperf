32461a334ad02985b7c4f3cf54571d052428ec00
Steve Yen <steve.yen@gmail.com>
2012-01-11 18:46:39 -0800

    First commit.

---
+++ cook.js
@@ -0,0 +1,46 @@
+#!/usr/bin/env node
+
+var fs = require("fs");
+
+var dirs = [];
+for (var i = 2; i < process.argv.length; i++) {
+  dirs[dirs.length] = process.argv[i];
+}
+
+for (var i in dirs) {
+  process_dir(dirs[i]);
+}
+
+function process_dir(data_path) {
+  var data_files = fs.readdirSync(data_path);
+
+  for (var i in data_files) {
+    console.log(data_files[i]);
+
+    var data = fs.readFileSync(data_path + "/" + data_files[i]);
+    var json = JSON.parse(data);
+
+    var n = 0;
+    var keys = {};
+    for (var j in json) {
+        n = n + 1;
+        var entry = json[j];
+        for (var key in entry) {
+            var val = entry[key];
+            var info = keys[key] = keys[key] || {};
+            info.count = (info.count || 0) + 1;
+            info.type  = (info.type || typeof(val));
+            if (info.type == "number") {
+              info.min = Math.min(val, info.min || Math.pow(2, 52));
+              info.max = Math.max(val, info.max || 0);
+            }
+        }
+    }
+
+    console.log("  bytes   : " + data.length);
+    console.log("  entries : " + n);
+    console.log(keys);
+  }
+}
+
+console.log("DONE")
