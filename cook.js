8136de24eb709892be333db2d3993e27651da3f0
Steve Yen <steve.yen@gmail.com>
2012-01-11 21:35:28 -0800

    Emit html

--- cook.js
+++ cook.js
@@ -2,39 +2,48 @@
 
 var fs = require("fs");
 
-var map_to_json = {}; // Key = path; val = json string.
-var map_to_info = {}; // Key = path; val = keys info.
+var o = console.log
+
+o("<html>");
+o("<body>")
+o("</body>")
+o("<script>")
+o("  var path_stat = {")
+
+var path_stat = {}; // Key = path; val = statistics of each path/file.
 
 function process_dir(dir) {
   fs.readdirSync(dir).forEach(function(fname) {
       var path = dir + "/" + fname;
-      console.log(path);
+      o("'" + path + "':");
 
-      var json = map_to_json[path] = fs.readFileSync(path);
-      var info = map_to_info[path] = {};
-      var keys = map_to_info[path].keys = {};
+      var json = fs.readFileSync(path);
+      var stat = path_stat[path] = {};
+      var keys = path_stat[path].keys = {};
 
       var rows = JSON.parse(json);
       for (var i in rows) {
         var row = rows[i];
         for (var key in row) {
           var val = row[key];
-          var key_info = keys[key] = keys[key] || {};
-          key_info.count = (key_info.count || 0) + 1;
-          key_info.type  = (key_info.type || typeof(val));
-          if (key_info.type == "number") {
-            key_info.min = Math.min(val, key_info.min || Math.pow(2, 52));
-            key_info.max = Math.max(val, key_info.max || 0);
+          var key_stat = keys[key] = keys[key] || {};
+          key_stat.count = (key_stat.count || 0) + 1;
+          key_stat.type  = (key_stat.type || typeof(val));
+          if (key_stat.type == "number") {
+            key_stat.min = Math.min(val, key_stat.min || Math.pow(2, 52));
+            key_stat.max = Math.max(val, key_stat.max || 0);
           }
         }
       }
-      info.count = rows.length;
+      stat.bytes = json.length;
+      stat.count = rows.length;
 
-      console.log("  bytes   : " + json.length);
-      console.log(info);
+      o(JSON.stringify(stat) + ",")
     });
 }
 
 process.argv.slice(2).forEach(process_dir);
 
-console.log("DONE")
+o("  };")
+o("</script>")
+o("</html>")
